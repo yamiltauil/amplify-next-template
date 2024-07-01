7d74587f26c3b010db74cf74903d81b35ab90dfa
yamiltauil <ytauil@itcrowdarg.com>
2024-07-01 13:59:18 -0300

    show user name

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function App() {
     <Authenticator>
    {({ signOut, user }) =>
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId} 'todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
