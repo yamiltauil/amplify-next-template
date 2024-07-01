1375203e74f3b7b2b24c149a5f49a598dea40863
yamiltauil <ytauil@itcrowdarg.com>
2024-07-01 14:58:10 -0300

    add authorization support

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+    }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,8 +18,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    // defaultAuthorizationMode: "apiKey",
-    defaultAuthorizationMode:'userPool',
+    defaultAuthorizationMode: 'userPool',
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
