1cbd689bd0598cd4c9c9db9dcbce85ca0925a12a
yamiltauil <ytauil@itcrowdarg.com>
2024-07-01 15:10:42 -0300

    change authorization

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,10 +7,9 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    }).authorization(allow => [allow.owner()]),
+  Todo: a.model({
+    content: a.string(),
+  }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,9 +18,9 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: 'userPool',
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    // apiKeyAuthorizationMode: {
+    //   expiresInDays: 30,
+    // },
   },
 });
 
