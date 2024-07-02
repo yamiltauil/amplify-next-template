17a64b875ddba7165422a8f8720a152a48524236
yamiltauil <ytauil@itcrowdarg.com>
2024-07-02 12:52:16 -0300

    add more features

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Authenticator } from '@aws-amplify/ui-react'
+import { Alert, Authenticator, Button, Divider, Heading, Loader, SearchField } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -16,15 +17,21 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [errr, setEr] = useState("")
+const [search, setSearch] = useState("")
 
    function listTodos() {
+     setEr("")
     try {
      client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-      console.log(fetch)
     } catch (error) {
-    console.log(error)
+     setEr(error)
+      console.log(error,2)
+    }finally{
+      setIsLoading(false)
     }
    
   }
@@ -34,17 +41,24 @@ export default function App() {
   }, [todos]);
 
 async  function createTodo() {
+    setEr("")
+  setIsLoading(true)
   try {
      await client.models.Todo.create({
       content: window.prompt("Todo content"),
     });
   } catch (e) {
     /* handle error */
-  console.log(e)
+  console.log(e,1)
+      setEr(e)
+  }finally{
+    listTodos()
   }
-  
-  }
+}
+
 async function updateTodo(id: string) {
+     setEr("")
+    setIsLoading(true)
   try {
     await client.models.Todo.update({
       id,
@@ -52,32 +66,76 @@ async function updateTodo(id: string) {
     });
   } catch (e) {
     /* handle error */
-    console.log(e)
-  }
+    console.log(e,3)
+    setEr(e)
+  }finally{
+    listTodos()
+    }
 }
-  
+
+const searchTasks = async (e:any) => {
+     setEr("")
+    setSearch(e.target.value)
+    if (e.target.value ==="") {
+      listTodos()
+    }else{
+  try {
+     const res = await client.models.Todo.list({
+      filter: {
+        content: {
+          contains: search,
+        },
+      },
+    });
+      setTodos(res.data)
+      console.log(res.data)
+  } catch (e) {
+    /* handle error */
+    console.log(e,4)
+        setEr(e)
+  }
+    }
+};
+
+  const ClearInput=()=>{
+    setSearch("")
+    listTodos()
+    
+  }
+
  async  function deleteTodo(id: string) {
+   setEr("")
+   setIsLoading(true)
     try {
-        console.log(id)
-  await  client.models.Todo.delete({ id })
+      await  client.models.Todo.delete({ id })
     } catch (e) {
       /* handle error */
-      console.log(e)
+    setEr(e)
+    }finally{
+     listTodos()
     }
-  
   }
 
   return (
     <Authenticator>
    {({ signOut, user }) =>
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content} <button onClick={() => updateTodo(todo.id)}>edit task</button>
- </li>         ))}
-      </ul>
+          {errr!=="" &&
+      <Alert isDismissible={true} variation='error'>{errr}</Alert>}
+       <Heading isTruncated={true} level={1}>{user?.signInDetails?.loginId}'s todos</Heading>
+         <Divider orientation='horizontal' />
+          <ul >
+           <SearchField placeholder="Search"  label="Search"  onChange={searchTasks} onClear={ClearInput}/>
+             {todos.map((todo) => (
+              <li key={todo.id} style={{ display: "flex", flexDirection: "column", gap: "1em" }}>{todo.content} 
+               <div style={{ display: "flex", flexDirection: "row", gap: "1em", justifyContent: "space-between" }}>
+                  <Button onClick={() => updateTodo(todo.id)} loadingText='Updating...'  isDisabled={isLoading}>edit task</Button>
+                  <Button onClick={() => deleteTodo(todo.id)} loadingText='Deleting... '  isDisabled={isLoading}>delete</Button>
+               </div>
+              </li> 
+             ))}
+           </ul>
+         <button onClick={createTodo}>+ new</button>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
@@ -85,10 +143,9 @@ async function updateTodo(id: string) {
           Review next steps of this tutorial.
         </a>
       </div>
-
       <button onClick={signOut}>Sign out</button>
     </main>
-      }
+    }
     </Authenticator>
   );
 }
